1d5ed7f01d9d004a4c84251aa7b54cb5e33180ed
LushoCallapa <luiscallapa2004@gmail.com>
2025-12-08 23:28:29 -0400

    CRUD

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUser, loginUser, getUsers } from "../controllers/userController";
+import { createUser, loginUser, getUsers, updateUser, deleteUser } from "../controllers/userController";
 
 const router = Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/", getUsers);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
